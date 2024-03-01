fbcceeb3865900d9b1d1a161c8deadc7ace4ad32
tydel <tydel@tydel.com>
2024-03-01 12:26:51 -0800

    changing to TLS v1.2 from TLS v1.0
    Signed-off-by: tydel <tydel@tydel.com>

--- libratbox/src/openssl.c
+++ libratbox/src/openssl.c
@@ -299,7 +299,7 @@ rb_init_ssl(void)
 	/* Disable SSLv2, make the client use our settings */
 	SSL_CTX_set_options(ssl_server_ctx, SSL_OP_NO_SSLv2 | SSL_OP_CIPHER_SERVER_PREFERENCE);
 
-	ssl_client_ctx = SSL_CTX_new(TLSv1_client_method());
+	ssl_client_ctx = SSL_CTX_new(TLSv1_2_client_method());
 
 	if(ssl_client_ctx == NULL)
 	{
